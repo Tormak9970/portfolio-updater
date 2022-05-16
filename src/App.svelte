15aef7bf0aae221b18e97fc03070740b52dfa724
Tormak <63308171+Tormak9970@users.noreply.github.com>
2022-05-15 19:07:26 -0500

    continued work on conversion

--- src/App.svelte
+++ src/App.svelte
@@ -7,53 +7,55 @@
 	import Setup from "./components/Setup.svelte";
 	import { config, renderIdx, state } from "./stores";
 	import { dialog, fs } from "@tauri-apps/api";
-	import { settingsCache, updateSettings } from "./Utils";
+	import { getConfig, setSettingsPath, settingsCache, settingsPath, updateSettings } from "./Utils";
 
 	let settings;
 	const components = [ Setup, Experience, EditorPage, Organizations, Art ];
 
 	onMount(async () => {
-		const settingsPath = await window.api.invoke("getSettingsPath");
+		await setSettingsPath();
 		settings = await fetch(settingsPath).then(response => { return response.json(); });
 
-		window.api.send("getConfig", settings.articlesFile);
+		const cfg = getConfig(settings.articlesFile);
 
-		await dialog.open({ directory: true }).then(async (dir) => {
-			if (dir) {
-				const files = await fs.readDir(dir as string);
-				const configFile = files.find((file) => file.name == "config.json");
-				
-				if (configFile) {
-					const contents = await fs.readTextFile(configFile.path);
-					const cfg = JSON.parse(contents);
+		if (!cfg) {
+			await dialog.open({ directory: true }).then(async (dir) => {
+				if (dir) {
+					const files = await fs.readDir(dir as string);
+					const configFile = files.find((file) => file.name == "config.json");
+					
+					if (configFile) {
+						const contents = await fs.readTextFile(configFile.path);
+						const cfg = JSON.parse(contents);
 
-					await updateSettings("configPath", configFile.path);
+						await updateSettings({prop: "configPath", data: configFile.path});
 
-					const data = [JSON.stringify(cfg), settingsCache.selCat, JSON.stringify(settingsCache.state)]
-					if (data[0]) {
-						$config = JSON.parse(data[0]);
-						$state = JSON.parse(data[2]);
+						const data = [JSON.stringify(cfg), settingsCache.selCat, JSON.stringify(settingsCache.state)]
+						if (data[0]) {
+							$config = JSON.parse(data[0]);
+							$state = JSON.parse(data[2]);
 
-						switch (data[1]) {
-							case "Experience":
-								$renderIdx = 1;
-								break;
-							case "Projects":
-								$renderIdx = 2;
-								break;
-							case "Organizations":
-								$renderIdx = 3;
-								break;
-							case "Art":
-								$renderIdx = 4;
-								break;
+							switch (data[1]) {
+								case "Experience":
+									$renderIdx = 1;
+									break;
+								case "Projects":
+									$renderIdx = 2;
+									break;
+								case "Organizations":
+									$renderIdx = 3;
+									break;
+								case "Art":
+									$renderIdx = 4;
+									break;
+							}
+						} else {
+							$renderIdx = 0;
 						}
-					} else {
-						$renderIdx = 0;
 					}
 				}
-			}
-		});
+			});
+		}
 	});
 </script>
 
--- src/Utils.ts
+++ src/Utils.ts
@@ -1,7 +1,7 @@
 import { dialog, fs, path } from "@tauri-apps/api";
 
 const devBuild = true;
-const settingsPath = await path.resolve(devBuild ? './public/settings.json' : "./resources/app/public/settings.json");
+export let settingsPath = "";
 export let settingsCache = {
   "configPath": "",
   "selCat": "",
@@ -27,56 +27,72 @@ export let settingsCache = {
   }
 }
 
-export async function updateSettings(event, data) {
-    await updateSettings(data.prop, data.value);
-}
-export async function getConfig(event, data) {
-    if (fs.existsSync(data)) {
-        const contents = fs.readFileSync(data);
-        const config = JSON.parse(contents);
+export async function setSettingsPath() { settingsPath = await path.resolve(devBuild ? './public/settings.json' : "./resources/app/public/settings.json"); }
 
-        window.webContents.send("config", [JSON.stringify(config), settingsCache.selCat, JSON.stringify(settingsCache.state)]);
-    } else {
-        window.webContents.send("config", [null, settingsCache.selCat, null]);
-    }
+export async function updateSettings(data: { prop: string, data: Object }) {
+  const settingsData = await fs.readTextFile(settingsPath);
+
+  const settings = JSON.parse(settingsData);
+  settings[data.prop] = data.data;
+  settingsCache[data.prop] = data.data;
+
+  await fs.writeFile({
+    path: settingsPath, 
+    contents: JSON.stringify(settings)
+  });
 }
-export function writeConfig(event, data) {
-    if (fs.existsSync(settingsCache.configPath)) {
-        fs.writeFileSync(settingsCache.configPath, data);
-    }
+export async function getConfig(data: string) {
+  await fs.readTextFile(data).then((contents) => {
+    const config = JSON.parse(contents);
+
+    return [JSON.stringify(config), settingsCache.selCat, JSON.stringify(settingsCache.state)];
+  }, () => {
+    return [null, settingsCache.selCat, null]
+  });
 }
-export async function getSettingsPath(event, data) { return settingsPath; });
+export async function writeConfig(data: string) {
+  try {
+    await fs.writeFile({
+      path: settingsCache.configPath, 
+      contents: data
+    })
+  } catch (e) {
+    console.log(e);
+  }
+}
 export async function uploadUrl(event, data) {
-    const url = data;
-    const name = url.substring(url.lastIndexOf("/") + 1);
+  const url = data;
+  const name = url.substring(url.lastIndexOf("/") + 1);
 
-    const response = await fetch(url);
-    const buffer = await response.buffer();
+  const response = await fetch(url);
+  const buffer = await response.buffer();
 
-    const imagesWebsiteDir = path.join(path.dirname(settingsCache.articlesFile), 'img', 'articles');
-    if (fs.existsSync(imagesWebsiteDir)) fs.mkdirSync(imagesWebsiteDir, { recursive: true });
-    fs.writeFileSync(path.join(imagesWebsiteDir, name), buffer);
+  const imagesWebsiteDir = path.join(path.dirname(settingsCache.articlesFile), 'img', 'articles');
+  if (fs.existsSync(imagesWebsiteDir)) fs.mkdirSync(imagesWebsiteDir, { recursive: true });
+  fs.writeFileSync(path.join(imagesWebsiteDir, name), buffer);
 
-    const imagesEditorDir = path.join(path.dirname(settingsPath), 'img', 'articles');
-    if (fs.existsSync(imagesEditorDir)) fs.mkdirSync(imagesEditorDir, { recursive: true });
-    fs.writeFileSync(path.join(imagesEditorDir, name), buffer);
+  // TODO: remove need for this
+  // const imagesEditorDir = path.join(path.dirname(settingsPath), 'img', 'articles');
+  // if (fs.existsSync(imagesEditorDir)) fs.mkdirSync(imagesEditorDir, { recursive: true });
+  // fs.writeFileSync(path.join(imagesEditorDir, name), buffer);
 
-    return JSON.stringify({ success: 1, file: { url: `./img/articles/${name}` } });
+  return JSON.stringify({ success: 1, file: { url: `./img/articles/${name}` } });
 }
 export async function uploadFile(event, data) {
-    const filePath = data;
-    const file = {
-        name: path.basename(filePath),
-        data: fs.readFileSync(filePath)
-    };
+  const filePath = data;
+  const file = {
+    name: path.basename(filePath),
+    data: fs.readFileSync(filePath)
+  };
 
-    const imagesWebsiteDir = path.join(path.dirname(settingsCache.articlesFile), 'img', 'articles');
-    if (fs.existsSync(imagesWebsiteDir)) fs.mkdirSync(imagesWebsiteDir, { recursive: true });
-    fs.writeFileSync(path.join(imagesWebsiteDir, file.name), file.data);
+  const imagesWebsiteDir = path.join(path.dirname(settingsCache.articlesFile), 'img', 'articles');
+  if (fs.existsSync(imagesWebsiteDir)) fs.mkdirSync(imagesWebsiteDir, { recursive: true });
+  fs.writeFileSync(path.join(imagesWebsiteDir, file.name), file.data);
 
-    const imagesEditorDir = path.join(path.dirname(settingsPath), 'img', 'articles');
-    if (fs.existsSync(imagesEditorDir)) fs.mkdirSync(imagesEditorDir, { recursive: true });
-    fs.writeFileSync(path.join(imagesEditorDir, file.name), file.data);
+  // TODO: remove need for this
+  // const imagesEditorDir = path.join(path.dirname(settingsPath), 'img', 'articles');
+  // if (fs.existsSync(imagesEditorDir)) fs.mkdirSync(imagesEditorDir, { recursive: true });
+  // fs.writeFileSync(path.join(imagesEditorDir, file.name), file.data);
 
-    return JSON.stringify({ success: 1, file: { url: `./img/articles/${file.name}` } });
+  return JSON.stringify({ success: 1, file: { url: `./img/articles/${file.name}` } });
 }
